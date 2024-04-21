033c86c68a7ae50999eb7a9576cf9a19f168bb05
Kaifarikzxc <147406196+Kaifarikzxc@users.noreply.github.com>
2024-04-21 22:30:45 +0300

    Add files via upload

---
+++ Pract4/Pract4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TravelAgency> travels = new List<TravelAgency>
+            {
+                new TravelAgency("Agenstvo1", 2000, "1", 1200000),
+                new TravelAgency("Agenstvo2", 1985, "2", 2000000),
+                new TravelAgency("Agenstvo3", 2014, "3", 32600000),
+                new TravelAgency("Agenstvo4", 2010, "4", 1900000)
+            };
+
+            Console.WriteLine("Введите название фирмы:");
+            string name = Console.ReadLine();
+            bool Find = false;
+
+            foreach (var travel in travels)
+            {
+                if (travel.Name == name)
+                {
+                    travel.Info();
+                    Find = true;
+                }
+            }
+
+            if (!Find)
+            {
+                Console.WriteLine("Фирма не найдена.");
+            }
+
+            decimal totalProfit = travels.Sum(x => x.Profit);
+            Console.WriteLine($"Суммарная прибыль всех фирм: {totalProfit}");
+        }
+    }
+}
---
+++ Pract4/Pract4/TravelAgency.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    class TravelAgency
+    {
+        private string name;
+        private int year;
+        private string inn;
+        private decimal profit;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public string Inn
+        {
+            get { return inn; }
+            set { inn = value; }
+        }
+
+        public decimal Profit
+        {
+            get { return profit; }
+            set { profit = value; }
+        }
+
+        public TravelAgency() { }
+
+        public TravelAgency(string name, int year, string inn, decimal profit)
+        {
+            Name = name;
+            Year = year;
+            Inn = inn;
+            Profit = profit;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Name: {Name}, Year: {Year}, Inn: {Inn}, Profit: {Profit}");
+        }
+    }
+}
